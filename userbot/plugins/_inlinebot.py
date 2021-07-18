cbea44c105b1ad10be4f4f944528d91826700673
Copycat-izz-opp <85601169+Copycat-izz-opp@users.noreply.github.com>
2021-07-18 10:32:26 +0530

    _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -25,7 +25,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
             buttons = [
                 (
                     custom.Button.inline("Stats", data="stats"),
-                    Button.url("Repo", "https://github.com/sandy1709/catuserbot"),
+                    Button.url("Repo", "https://github.com/Copycat-izz-opp/CopyCat_Spam_Bot"),
                 )
             ]
             if CAT_IMG and CAT_IMG.endswith((".jpg", ".png")):
